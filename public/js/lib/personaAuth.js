4d007ff748009d5f37bf47a4d65915296a9be1e9
Cheng Zhou <zhouc@advisory.com>
2014-02-12 17:18:38 -0600

    added parameters to selectively enable/disable browserid auth

--- public/js/lib/personaAuth.js
+++ public/js/lib/personaAuth.js
@@ -1,19 +1,16 @@
 $(document).ready(function() {
   $("#browserid").click(function() {
-    console.log('clicked on persona login');
     navigator.id.get(function(assertion) {
       if (assertion) {
         $('input[name="assertion"]').val(assertion);
         $('form[action="/auth/browserid"]').submit();
       } else {
-        console.log('bad assertion');
         location.reload();
       }
     });
   });
   
   $("#logout").click(function() {
-    console.log('logout');
     navigator.id.logout();
   })
 });
